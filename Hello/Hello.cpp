5659af4875aba92c59c9d9285b9a3cdb3dad5cfd
Kaylen Travis Pillay <kaylentravispillay@gmail.com>
2017-02-05 19:18:10 +0200

    Added my first c++ project

---
+++ Hello/Hello.cpp
@@ -0,0 +1,20 @@
+/***************************************************
+** File:      Hello.cpp
+** Project:   My First C++ Program, Hello World
+** Author:    Kaylen Travis Pillay
+** Date:      05/02/2017
+** E-mail:    kaylentp@ktpsolutions.co.za
+**
+**  This file contains the main driver program for HelloWorld.
+** This program displays "Hello World!" to the screen.
+**
+*****************************************************/
+
+#include <iostream>
+
+int main(){
+
+ std::cout << "Hello World\n";
+
+ return (0);
+}
